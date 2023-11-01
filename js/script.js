7ddd7b8e0f4aea7c11c518b94abafac98247b6b8
Estiber <gryfo.16@hotmail.es>
2023-11-01 17:29:03 -0500

    Entrega 6 con funcionalidad de navegaciones

--- js/script.js
+++ js/script.js
@@ -1,22 +1,4 @@
-document.getElementById("loginButton").addEventListener("click", function () {
-  document.getElementById("loginPopup").style.display = "block";
-  document.getElementById("blurBackground").style.display = "block";
-});
 
-document.getElementById("closeLoginPopup").addEventListener("click", function () {
-  document.getElementById("loginPopup").style.display = "none";
-  document.getElementById("blurBackground").style.display = "none";
-});
-
-document.getElementById("registroButton").addEventListener("click", function () {
-  document.getElementById("registroPopup").style.display = "block";
-  document.getElementById("blurBackground").style.display = "block";
-});
-
-document.getElementById("closeRegistroPopup").addEventListener("click", function () {
-  document.getElementById("registroPopup").style.display = "none";
-  document.getElementById("blurBackground").style.display = "none";
-});
 
 document.getElementById("menuIcon").addEventListener("click", function () {
   document.querySelector(".nav-links").classList.toggle("show-links");
