b4f8dec8495433b3a368687ad8c073c9d5d68651
Estiber <gryfo.16@hotmail.es>
2023-11-01 17:24:46 -0500

    Entrega 5 con funcionalidad de formulario de contacto

--- js/script.js
+++ js/script.js
@@ -1,1 +1,29 @@
-// Aqui se van a añadir los eventos de menu hamburugesa y formulario
+document.getElementById("loginButton").addEventListener("click", function () {
+  document.getElementById("loginPopup").style.display = "block";
+  document.getElementById("blurBackground").style.display = "block";
+});
+
+document.getElementById("closeLoginPopup").addEventListener("click", function () {
+  document.getElementById("loginPopup").style.display = "none";
+  document.getElementById("blurBackground").style.display = "none";
+});
+
+document.getElementById("registroButton").addEventListener("click", function () {
+  document.getElementById("registroPopup").style.display = "block";
+  document.getElementById("blurBackground").style.display = "block";
+});
+
+document.getElementById("closeRegistroPopup").addEventListener("click", function () {
+  document.getElementById("registroPopup").style.display = "none";
+  document.getElementById("blurBackground").style.display = "none";
+});
+
+document.getElementById("menuIcon").addEventListener("click", function () {
+  document.querySelector(".nav-links").classList.toggle("show-links");
+  document.getElementById("blurBackground").style.display = "block";
+});
+
+document.getElementById("blurBackground").addEventListener("click", function () {
+  document.querySelector(".nav-links").classList.remove("show-links");
+  document.getElementById("blurBackground").style.display = "none";
+});
